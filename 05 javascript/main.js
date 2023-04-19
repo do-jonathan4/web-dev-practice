71645eb742db3cfa81774bcdb76c8a626f60b5eb
do-jonathan4 <d.johnt45@gmail.com>
2023-04-19 01:42:21 -0700

    arrays

--- 05 javascript/main.js
+++ 05 javascript/main.js
@@ -25,3 +25,41 @@ console.log('My name is ' + name + ' and I am ' + age);
 // Template literal (better)
 console.log(`My name is ${name} and I am ${age}`);
 
+// String methods & properties
+const s = 'Hello World';
+let val;
+// Get length
+val = s.length;
+// Change case
+val = s.toUpperCase();
+val = s.toLowerCase();
+// Get sub string
+val = s.substring(0, 5);
+// Split into array
+val = s.split('');
+
+// ARRAYS - Store multiple values in a variable
+const numbers = [1,2,3,4,5];
+const fruits = ['apples', 'oranges', 'pears', 'grapes'];
+console.log(numbers, fruit);
+
+// Get one value - Arrays start at 0
+console.log(fruits[1]);
+
+// Add value
+fruits[4] = 'blueberries';
+
+// Add value using push()
+fruits.push('strawberries');
+
+// Add to beginning
+fruits.unshift('mangos');
+
+// Remove last value
+fruits.pop();
+
+// // Check if array
+console.log(Array.isArray(fruits));
+
+// // Get index
+console.log(fruits.indexOf('oranges'));
