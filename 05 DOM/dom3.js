a98cb58d16523e1ea17d2c17cc5822bf2a5798f5
do-jonathan4 <d.johnt45@gmail.com>
2023-04-25 21:04:20 -0700

    keyboard, input events

--- 05 DOM/dom3.js
+++ 05 DOM/dom3.js
@@ -28,10 +28,29 @@
 // box.addEventListener('mouseout', runEvent)
 // box.addEventListener('mousemove', runEvent)
 
+const itemInput = document.querySelector('input[type="text"]')
+const form = document.querySelector('form')
+const select = document.querySelector('select')
 
+// itemInput.addEventListener('keydown', runEvent)
+// itemInput.addEventListener('keyup', runEvent)
+// itemInput.addEventListener('keypress', runEvent)
+// itemInput.addEventListener('focus', runEvent)
+// itemInput.addEventListener('blur', runEvent)
+// itemInput.addEventListener('cut', runEvent)
+// itemInput.addEventListener('paste', runEvent)
+// itemInput.addEventListener('input', runEvent)
+// select.addEventListener('change', runEvent)
+// select.addEventListener('input', runEvent)
+form.addEventListener('submit', runEvent)
 
 function runEvent (e) {
+    e.preventDefault()
     console.log('EVENT TYPE: ' + e.type);
+
+    // console.log(e.target.value);
+
+    // document.getElementById('output').innerHTML = e.target.value
 
     // output.innerHTML = '<h3>MouseX: '+e.offsetX+' </h3><h3>MouseY: '+e.offsetY+'</h3>'
 }
