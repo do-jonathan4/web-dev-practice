e1fdd6cab78f1bd952f62abbb5100cb3715d2d6c
do-jonathan4 <d.johnt45@gmail.com>
2023-05-19 09:42:55 -0700

    CreatPost, callback, promise

--- 11 async/callback.js
+++ 11 async/callback.js
@@ -1,4 +1,4 @@
-const post = [
+const posts = [
     {title: 'Post One', body: 'This is post one'},
     {title: 'Post Two', body: 'This is post two'},
 ]
@@ -6,11 +6,19 @@ const post = [
 function getPosts() {
     setTimeout(() => {
         let output = ''
-        post.forEach((post, index) => {
+        posts.forEach((post, index) => {
             output += `<li>${post.title}</li>`
             document.body.innerHTML = output
         })
     }, 1000)
 }
 
-getPosts()
+function createPost(post, callback) {
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000)
+}
+
+// getPosts()
+createPost({title: 'Post Three', body: 'This is post three'}, getPosts)
