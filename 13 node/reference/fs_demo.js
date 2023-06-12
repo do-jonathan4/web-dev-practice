483ca6879b72623a65b58bd3b69f89efdefcdc9c
do-jonathan4 <d.johnt45@gmail.com>
2023-06-12 16:22:54 -0700

    fs

--- 13 node/reference/fs_demo.js
+++ 13 node/reference/fs_demo.js
@@ -1,14 +1,40 @@
 const fs = require('fs')
 const path = require('path')
 
-//Create folder
+// Create folder
 fs.mkdir(path.join(__dirname, 'test'), {}, err => {
     if (err) throw err
     console.log('Folder Created');
 })
 
-//Create and write to file
-fs.mkdir(path.join(__dirname, 'test'), {}, err => {
-    if (err) throw err
-    console.log('Folder Created');
+// Create and write to file
+fs.writeFile(path.join(__dirname, 'test', 'hello.txt'), 
+    'Hello World', 
+    err => {
+        if (err) throw err
+        console.log('File Created');
+})
+
+// Append file
+fs.appendFile(path.join(__dirname, 'test', 'hello.txt'), 
+    'This is cool', 
+    err => {
+        if (err) throw err
+        console.log('File written to ...');
+})
+
+//Read file
+fs.readFile(path.join(__dirname, 'test', 'hello.txt'), 
+    'utf8', 
+    (err, data) => {
+        if (err) throw err
+        console.log(data);
+})
+
+//Rename file
+fs.rename(path.join(__dirname, 'test', 'hello.txt'), 
+    path.join(__dirname, 'test', 'helloworld.txt'), 
+    (err) => {
+        if (err) throw err
+        console.log('file renamed');
 })
