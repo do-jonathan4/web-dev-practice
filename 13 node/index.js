e7bb3af49068e5ca9613d9026d571d907e07fa76
do-jonathan4 <d.johnt45@gmail.com>
2023-06-16 15:25:08 -0700

    server, err res

--- 13 node/index.js
+++ 13 node/index.js
@@ -39,6 +39,7 @@ const server = http.createServer((req, res) => {
     let extname = path.extname(filePath)
     let contentType = 'text/html'
 
+    // Check ext and set content type
     switch(extname) {
         case '.js':
             contentType = 'text/javascript'
@@ -55,8 +56,32 @@ const server = http.createServer((req, res) => {
         case '.jpg':
             contentType = 'image/jpg'
             break
-
     }
+    if (contentType == "text/html" && extname == "") filePath += ".html";
+
+    // Read File
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                // Page not found
+                fs.readFile(path.join(__dirname, 
+                    'public',
+                    '404.html'
+                    ), (err, content) => {
+                        res.writeHead(200, {'Content-Type': contentType})
+                        res.end(content, 'utf8')
+                    })
+            } else {
+                // Some server error
+                res.writeHead(500)
+                res.end(`Server Error: ${err.code}`)
+            }
+        } else {
+            // Success
+            res.writeHead(200, {'Content-Type': contentType})
+            res.end(content, 'utf8')
+        }
+    })
 })
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
