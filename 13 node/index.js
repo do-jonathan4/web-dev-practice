f0aae2738e21b8bcf3617fffd00b00b07e4bcdb9
do-jonathan4 <d.johnt45@gmail.com>
2023-06-15 23:32:37 -0700

    moved logger, http, nodemon, server

--- 13 node/index.js
+++ 13 node/index.js
@@ -1,7 +1,39 @@
-const Logger = require('./logger')
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+const { stringify } = require('querystring')
+const PORT = process.env.port || 5000
 
-const logger = new Logger()
+const server = http.createServer((req, res) => {
+    // if (req.url === '/') {
+    //     fs.readFile(
+    //         path.join(__dirname, 'public', 'index.html'), 
+    //         (err, content) => {
+    //             if (err) throw err
+    //             res.writeHead(200, {'Content-Type': 'text/html'})
+    //             res.end(content)
+    //     })
+    // }
+    // if (req.url === '/about') {
+    //     fs.readFile(
+    //         path.join(__dirname, 'public', 'about.html'), 
+    //         (err, content) => {
+    //             if (err) throw err
+    //             res.writeHead(200, {'Content-Type': 'text/html'})
+    //             res.end(content)
+    //     })
+    // }
+    // if (req.url === '/api/users') {
+    //     const users = [
+    //         {name: 'Bob', age: 40},
+    //         {name: 'Bill', age: 30}
+    //     ]
+    //     res.writeHead(200, {'Content-Type': 'application/json'})
+    //     res.end(JSON.stringify(users))
+    // }
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+    console.log(filePath);
+    res.end()
+})
 
-logger.on('message', data => console.log('Called Listener', data))
-
-logger.log('Hello World')
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
