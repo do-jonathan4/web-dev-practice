d97ee04e90037332202681ea514ff52b5a8acac3
do-jonathan4 <d.johnt45@gmail.com>
2023-07-14 10:20:17 -0700

    added controller routes and functions

--- 16 mern/backend/routes/goalRoutes.js
+++ 16 mern/backend/routes/goalRoutes.js
@@ -1,20 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controllers/goalsController')
 
-router.get('/', (req, res) => {
-    res.status(200).json({message: `Get Goals`})
-})
-
-router.post('/', (req, res) => {
-    res.status(200).json({message: `Set Goals`})
-})
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({message: `Updated Goal ${req.params.id}`})
-})
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({message: `Deleted Goal ${req.params.id}`})
-})
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 module.exports = router
