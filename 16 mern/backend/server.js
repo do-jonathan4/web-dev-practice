bd14417f8d64088c250b216743f904068a3fe774
do-jonathan4 <d.johnt45@gmail.com>
2023-07-17 12:33:06 -0700

    error handling, express async handling

--- 16 mern/backend/server.js
+++ 16 mern/backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const {errorHandler} = require('./middleware/errorMiddleware')
 
 const app = express()
 
@@ -8,5 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
