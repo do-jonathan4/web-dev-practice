b3b7eb3b181a0133985843dcb0111235e7a64de9
do-jonathan4 <d.johnt45@gmail.com>
2023-07-28 15:29:19 -0700

    post, put, delete requests

--- 17 project-3/backend/utils/routes/router.js
+++ 17 project-3/backend/utils/routes/router.js
@@ -7,16 +7,43 @@ router.get('/', asyncHandler(async (req, res) => {
     const list = await Model.find()
     res.status(200).json(list)
 }))
+
 router.post('/', asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Hello'})
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error ('Please include text')
+    }
+
+    await Model.create({item: req.body.text})
+    res.status(200).json({message: `Created item: ${req.body.text}`})
 }))
+
 router.put('/:id', asyncHandler(async (req, res) => {
-    const task = await Model.findById(req.params.id)
-    res.status(200).json({message: 'Hello'})
+    const id = await Model.findById(req.params.id)
+
+    if (!id) {
+        res.status(400)
+        throw new Error (`Can not find item with ID: ${id}`)
+    }
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error ('Please include text')
+    }
+
+    await Model.findByIdAndUpdate(id, {item: req.body.text})
+    res.status(200).json({message: `Updated item to: ${req.body.text}`})
 }))
+
 router.delete('/:id', asyncHandler(async (req, res) => {
-    const task = await Model.findById(req.params.id)
-    res.status(200).json({message: 'Hello'})
+    const id = await Model.findById(req.params.id)
+
+    if (!id) {
+        res.status(400)
+        throw new Error (`Can not find item with ID: ${id}`)
+    }
+
+    await Model.findByIdAndDelete(id)
+    res.status(200).json({message: 'Deleted item successfully'})
 }))
 
 module.exports = router
