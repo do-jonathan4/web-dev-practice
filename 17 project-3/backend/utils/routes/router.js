b4e559f3bec2d35f8a8a6dc2fc20cf925432a5d8
do-jonathan4 <d.johnt45@gmail.com>
2023-07-27 19:05:18 -0700

    get request

--- 17 project-3/backend/utils/routes/router.js
+++ 17 project-3/backend/utils/routes/router.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler')
+const Model = require('../config/schema')
 const router = express.Router()
 
 router.get('/', asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Hello'})
+    const list = await Model.find()
+    res.status(200).json(list)
 }))
 router.post('/', asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Hello'})
 }))
 router.put('/:id', asyncHandler(async (req, res) => {
+    const task = await Model.findById(req.params.id)
     res.status(200).json({message: 'Hello'})
 }))
 router.delete('/:id', asyncHandler(async (req, res) => {
+    const task = await Model.findById(req.params.id)
     res.status(200).json({message: 'Hello'})
 }))
 
