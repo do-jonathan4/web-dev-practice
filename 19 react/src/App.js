6dbf5782caebe9798eb7f4999025fadf532b0019
do-jonathan4 <d.johnt45@gmail.com>
2023-08-16 17:36:02 -0700

    add task

--- 19 react/src/App.js
+++ 19 react/src/App.js
@@ -25,6 +25,11 @@ const App = () => {
     },
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    setTasks([...tasks, { id, ...task }]);
+  };
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -42,7 +47,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
