e12ab04beb42f09f4e9ae304cfc57d61264ff241
do-jonathan4 <d.johnt45@gmail.com>
2023-09-04 09:04:47 -0700

    tested functions

--- 20 jest/functions.test.js
+++ 20 jest/functions.test.js
@@ -10,3 +10,24 @@ test("Adds 2 + 2 to NOT equal 5", () => {
 test("Should be null", () => {
   expect(functions.isNull()).toBeNull();
 });
+
+test("Should be falsy", () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+test("User should be Brad Smith object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Brad",
+    lastName: "Smith",
+  });
+});
+
+test("Should be under 1600", () => {
+  const a = 800;
+  const b = 700;
+  expect(a + b).toBeLessThan(1600);
+});
+
+test("There is no I in team", () => {
+  expect("team").not.toMatch(/I/);
+});
