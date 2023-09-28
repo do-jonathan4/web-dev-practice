55d2a39340a19c0fcb41ff593f8e15acfecaf976
do-jonathan4 <d.johnt45@gmail.com>
2023-09-28 16:29:01 -0700

    naming convention, coverage

--- 21 react-testing/src/components/greet/Greet.test.tsx
+++ 21 react-testing/src/components/greet/Greet.test.tsx
@@ -5,15 +5,18 @@ import { Greet } from "./greet";
  * Greet should render text hello and if a name is passed into the component
  * It should render hello followed by the name
  */
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const text = screen.getByText(/hEllO/i);
+    expect(text).toBeInTheDocument();
+  });
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const text = screen.getByText(/hEllO/i);
-  expect(text).toBeInTheDocument();
-});
-
-test("Greet renders with a name", () => {
-  render(<Greet name="Joe" />);
-  const text = screen.getByText(/hEllO Joe/i);
-  expect(text).toBeInTheDocument();
+  describe("Nested", () => {
+    test("renders with a name", () => {
+      render(<Greet name="Joe" />);
+      const text = screen.getByText(/hEllO Joe/i);
+      expect(text).toBeInTheDocument();
+    });
+  });
 });
