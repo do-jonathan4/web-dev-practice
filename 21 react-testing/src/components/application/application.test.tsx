dcfb97233c1daf5a8c130a7549db9bc05abd0ba9
do-jonathan4 <d.johnt45@gmail.com>
2023-10-02 21:48:20 -0700

    getByPlaceholderText, getByText, getByDisplayValue, getByAltText, getByTitle

--- 21 react-testing/src/components/application/application.test.tsx
+++ 21 react-testing/src/components/application/application.test.tsx
@@ -11,11 +11,26 @@ describe("Application", () => {
     const sectionHeading = screen.getByRole("heading", { level: 2 });
     expect(sectionHeading).toBeInTheDocument();
 
+    const paragraphElement = screen.getByText("All fields are mandatory");
+    expect(paragraphElement).toBeInTheDocument();
+
+    const imageElement = screen.getByAltText("a person with a laptop");
+    expect(imageElement).toBeInTheDocument();
+
+    const closeElement = screen.getByTitle("close");
+    expect(closeElement).toBeInTheDocument();
+
     const nameElement = screen.getByRole("textbox", { name: "Name" });
     expect(nameElement).toBeInTheDocument();
 
     const nameElement2 = screen.getByLabelText("Name", { selector: "input" });
     expect(nameElement2).toBeInTheDocument();
+
+    const nameElement3 = screen.getByPlaceholderText("Fullname");
+    expect(nameElement3).toBeInTheDocument();
+
+    const nameElement4 = screen.getByDisplayValue("John");
+    expect(nameElement4).toBeInTheDocument();
 
     const bioElement = screen.getByRole("textbox", { name: "Bio" });
     expect(bioElement).toBeInTheDocument();
