f09ca3e70a67cc33a24952e5bf9eb796fae0c176
do-jonathan4 <d.johnt45@gmail.com>
2023-10-12 09:42:57 -0700

    tech container, classname

--- 22 react-portfolio/src/components/Tech.js
+++ 22 react-portfolio/src/components/Tech.js
@@ -1,34 +1,35 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 function Tech() {
   return (
-    <div id="tech" class="bg-secondary text-white p-5">
-      <div class="container p-4">
-        <h2 class="text-center pt-3">Technologies</h2>
-        <div class="container p-4 fs-5">
-          <ul class="list-unstyled">
+    <div id="tech" className="bg-secondary text-white p-5">
+      <Container className="p-4">
+        <h2 className="text-center pt-3">Technologies</h2>
+        <Container className="p-4 fs-5">
+          <ul className="list-unstyled">
             <li>
-              <span class="lead">Programing Languages:</span>
+              <span className="lead">Programing Languages:</span>
               JavaScript, ES6+, Node, Express, PostgreSQL, React, Redux
             </li>
             <li>
-              <span class="lead">Tools/Technologies:</span>
+              <span className="lead">Tools/Technologies:</span>
               Git, GitHub, Bootstrap, CLI, OOP, HTTP, AJAX, Axios, APIs, Webpack
             </li>
           </ul>
+        </Container>
+        <div className="p-4">
+          <i className="px-2 fa-brands fa-js fa-lg"></i>
+          <i className="px-2 fa-brands fa-node-js fa-lg"></i>
+          <i className="px-2 fa-brands fa-react fa-lg"></i>
+          <i className="px-2 fa-solid fa-terminal fa-lg"></i>
+          <i className="px-2 fa-solid fa-database fa-lg"></i>
+          <i className="px-2 fa-brands fa-bootstrap fa-lg"></i>
+          <i className="px-2 fa-brands fa-npm fa-lg"></i>
+          <i className="px-2 fa-brands fa-git fa-lg"></i>
+          <i className="px-2 fa-brands fa-github fa-lg"></i>
         </div>
-        <div class="p-4">
-          <i class="px-2 fa-brands fa-js fa-lg"></i>
-          <i class="px-2 fa-brands fa-node-js fa-lg"></i>
-          <i class="px-2 fa-brands fa-react fa-lg"></i>
-          <i class="px-2 fa-solid fa-terminal fa-lg"></i>
-          <i class="px-2 fa-solid fa-database fa-lg"></i>
-          <i class="px-2 fa-brands fa-bootstrap fa-lg"></i>
-          <i class="px-2 fa-brands fa-npm fa-lg"></i>
-          <i class="px-2 fa-brands fa-git fa-lg"></i>
-          <i class="px-2 fa-brands fa-github fa-lg"></i>
-        </div>
-      </div>
+      </Container>
     </div>
   );
 }
