39df47b0d3f5668c21902812e6cf3e73f03c4259
do-jonathan4 <d.johnt45@gmail.com>
2023-10-13 16:59:56 -0700

    app cards

--- 22 react-portfolio/src/components/Projects.js
+++ 22 react-portfolio/src/components/Projects.js
@@ -1,13 +1,31 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import AppCard from "./AppCard";
 
 function Projects() {
   const projectData = [
-    { id: 1, img: null, title: "Project 1" },
-    { id: 2, img: null, title: "Project 1" },
-    { id: 3, img: null, title: "Project 1" },
-    { id: 4, img: null, title: "Project 1" },
+    {
+      id: 1,
+      img: "https://via.placeholder.com/600/b0f7cc",
+      title: "Project 1",
+    },
+    {
+      id: 2,
+      img: "https://via.placeholder.com/600/b0f7cc",
+      title: "Project 2",
+    },
+    {
+      id: 3,
+      img: "https://via.placeholder.com/600/b0f7cc",
+      title: "Project 3",
+    },
+    {
+      id: 4,
+      img: "https://via.placeholder.com/600/b0f7cc",
+      title: "Project 4",
+    },
   ];
+
   return (
     <div id="projects" class="bg-light p-5">
       <Container class="container p-4">
@@ -15,7 +33,11 @@ function Projects() {
         <div
           id="projectContainer"
           class="p-4 d-flex flex-wrap align-items-center justify-content-around"
-        ></div>
+        >
+          {projectData.map((x) => (
+            <AppCard key={x.id} data={x} />
+          ))}
+        </div>
       </Container>
     </div>
   );
