f98935a19f1e2293222d58cf0255195911d514d1
do-jonathan4 <d.johnt45@gmail.com>
2023-10-13 09:33:41 -0700

    fake project data

--- 22 react-portfolio/src/components/Projects.js
+++ 22 react-portfolio/src/components/Projects.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 function Projects() {
+  const projectData = [
+    { id: 1, img: null, title: "Project 1" },
+    { id: 2, img: null, title: "Project 1" },
+    { id: 3, img: null, title: "Project 1" },
+    { id: 4, img: null, title: "Project 1" },
+  ];
   return (
     <div id="projects" class="bg-light p-5">
-      <div class="container p-4">
+      <Container class="container p-4">
         <h2 class="text-center pt-3">Projects</h2>
         <div
           id="projectContainer"
           class="p-4 d-flex flex-wrap align-items-center justify-content-around"
         ></div>
-      </div>
+      </Container>
     </div>
   );
 }
