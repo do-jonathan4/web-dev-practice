2f05b7714dc690507a9aca0c012ad8a1ca98a6de
do-jonathan4 <d.johnt45@gmail.com>
2023-11-02 15:43:36 -0700

    useAuth, components

--- 24 pro-project-1/src/App.js
+++ 24 pro-project-1/src/App.js
@@ -1,10 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+import { auth, firestore } from "./firebase";
+import ChatRoom from "./components/ChatRoom";
+import SignIn from "./components/SignIn";
+
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header"></header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
 }
