3bedbbac48bbad00b74f46d1a480e36b0e73e55c
do-jonathan4 <d.johnt45@gmail.com>
2023-11-03 15:46:33 -0700

    signin signout comp

---
+++ 24 pro-project-1/src/components/SignOut.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { auth } from "../firebase";
+
+const SignOut = () => {
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
+  );
+};
+
+export default SignOut;
