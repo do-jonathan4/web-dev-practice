795ba54cbcf00b346b91f6ac272ef7dbc961b74d
do-jonathan4 <d.johnt45@gmail.com>
2023-11-07 21:11:42 -0800

    return fetch messages

--- 24 pro-project-1/src/components/ChatRoom.js
+++ 24 pro-project-1/src/components/ChatRoom.js
@@ -1,9 +1,38 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
+} from "firebase/firestore";
+
+import { db } from "../firebase";
 import SendMessage from "./SendMessage";
 
 const ChatRoom = () => {
   const scroll = useRef();
+  const [messages, setMessages] = useState([]);
 
+  useEffect(() => {
+    const q = query(
+      collection(db, "messages"),
+      orderBy("createdAt", "desc"),
+      limit(30)
+    );
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      const fetchedMessages = [];
+      QuerySnapshot.forEach((doc) => {
+        fetchedMessages.push({ ...doc.data(), id: doc.id });
+      });
+      const sortedMessages = fetchedMessages.sort(
+        (a, b) => a.createdAt - b.createdAt
+      );
+      setMessages(sortedMessages);
+    });
+
+    return () => unsubscribe;
+  }, []);
   //message array
   // query
   // order by X
